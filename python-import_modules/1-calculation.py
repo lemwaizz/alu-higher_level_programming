348ab15398b79091efadfc226b0b327d731f85f9
lemwaizz <mwaidennis26@gmail.com>
2023-11-01 07:44:34 -0700

    fix

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,4 +3,4 @@ if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print(" {} + {} = {}\n".format(a, b, add(a,b)),"{} - {} = {}\n".format(a, b, sub(a,b)).lstrip(),"{} * {} = {}\n".format(a, b, mul(a,b)).lstrip(),"{} / {} = {}\n".format(a, b, div(a,b)).lstrip())
+    print("{} + {} = {}".format(a, b, add(a,b)),"\n{} - {} = {}".format(a, b, sub(a,b)),"\n{} * {} = {}".format(a, b, mul(a,b)),"\n{} / {} = {}".format(a, b, div(a,b)))
