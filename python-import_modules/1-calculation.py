6084bd9d2c36720560ed07df03f55ac90f27dbf1
lemwaizz <mwaidennis26@gmail.com>
2023-11-01 07:31:16 -0700

    swish

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print(" {} + {} = {}\n".format(a, b, add(a,b)),"{} - {} = {}\n".format(a, b, sub(a,b)).lstrip(),"{} * {} = {}\n".format(a, b, mul(a,b)).lstrip(),"{} / {} = {}\n".format(a, b, div(a,b)).lstrip())
