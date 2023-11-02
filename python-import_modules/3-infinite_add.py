7c3b33595fb76b89ee1e0a7fee290d88d071eef2
lemwaizz <mwaidennis26@gmail.com>
2023-11-02 12:04:31 -0700

    argv[1:]=[int(x) for x in argv[1:]]

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -6,6 +6,6 @@ from sys import argv
 if __name__ == '__main__':
     def my_func(argv):
         argv[1:] = [int(x) for x in argv[1:]]
-        print(sum(argv[1:]))            
+        print(sum(argv[1:]))
 
 my_func(argv)
