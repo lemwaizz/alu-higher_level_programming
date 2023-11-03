9f02fe09c4e9b34e9393a6e42876481e3d84eb2e
lemwaizz <mwaidennis26@gmail.com>
2023-11-03 10:37:20 -0700

    chr(ord(int))

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,4 +2,6 @@
 def uppercase(str):
     for ch in str:
         if ord(ch) >= 97 and ord(ch) <= 122:
-            print("{}".format((chr(ord(ch)) - 32)), end='')
+            char = chr(ord(ch) - 32)
+        print("{}".format(char), end='')
+    
