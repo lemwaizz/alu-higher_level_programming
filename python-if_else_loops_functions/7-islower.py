ed4e210cc2ce09b2f12d3e8f6b448081d15ad144
lemwaizz <mwaidennis26@gmail.com>
2023-11-03 10:09:05 -0700

    ord

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    def islower(c):
-        if ord(c) >= 97 and ord(c) <= 122 :
-            return True
-        else:
-            return False
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122 :
+        return True
+    else:
+        return False
