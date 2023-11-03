9c6097f55f7cd717f604c94edb4e50eaeb70f52c
lemwaizz <mwaidennis26@gmail.com>
2023-11-02 20:20:38 -0700

    ast

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 import ast
+b = []
 if __name__ == '__main__':
     with open('hidden_4.py') as f:
         tree = f.read()
-module = ast.parse(tree)
-functions = [node for node in module.body if isinstance(node, ast.FunctionDef)]
-for f in functions:
-    a = str(f.name)
-    b = []
-    if a.startswith('__'):
-        pass
-    else:
-        b.append(a)
-        b.sort()
-    for i in range(len(b)):
-        print(b[i])
+        module = ast.parse(tree)
+        functions = [node for node in module.body if isinstance(node, ast.FunctionDef)]
+        for f in functions:
+            a = str(f.name)
+            if a.startswith('__'):
+                pass
+            else:
+                b.append(a)
+        b = sorted(b)
+        for i in range(len(b)):
+            print(b[i])
