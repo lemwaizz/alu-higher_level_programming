b588d2681936242c83a38ebeb6aa32ca458a2b57
lemwaizz <mwaidennis26@gmail.com>
2023-11-03 09:57:22 -0700

    aight

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def islower(c):
+    if ord('c') in range(97,123):
+        return True
+    elif ord('c') in range(65,91):
+        return False
+    else:
+        return False
