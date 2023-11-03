4d9a3df74b0e8e5bf42b4da1b5ec13cf511e0a99
lemwaizz <mwaidennis26@gmail.com>
2023-11-02 19:00:00 -0700

    ast

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import uncompyle6, ast
+import uncompyle6
+import ast
 if __name__ == '__main__':
     with open('hidden_4.py', 'w') as f:
         uncompyle6.decompile_file('hidden_4.pyc', f)
 with open('hidden_4.py') as f:
     tree = f.read()
 module = ast.parse(tree)
-function_definitions = [node for node in module.body if isinstance(node, ast.FunctionDef)]
-for f in function_definitions:
+functions = [node for node in module.body if isinstance(node, ast.FunctionDef)]
+for f in functions:
     a = str(f.name)
     b = []
     if a.startswith('__'):
@@ -17,4 +18,3 @@ for f in function_definitions:
         b.sort()
     for i in range(len(b)):
         print(b[i])
-        
