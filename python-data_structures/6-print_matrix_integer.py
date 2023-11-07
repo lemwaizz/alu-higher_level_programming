ed668e8ff79b166d68992395bcb884a8635fd161
lemwaizz <mwaidennis26@gmail.com>
2023-11-07 15:07:53 -0800

    translate

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for c in range((len(my_list[0]) - 1)):
+        for i in range((len(my_list)) - 1):
+            print("{:d}".format(my_list[i][c]))
