75e5bfd901b7724e342de706c36f412fb87c9720
lemwaizz <mwaidennis26@gmail.com>
2023-11-07 06:38:55 -0800

    reversed

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0:
-        print(my_list)
-    else:
+    if len(my_list) > 0:
         new_list = list(reversed(my_list))
         for listmethod in new_list:
             print("{:d}".format(listmethod))
+    else:
+        raise ValueError("list must contain items")
