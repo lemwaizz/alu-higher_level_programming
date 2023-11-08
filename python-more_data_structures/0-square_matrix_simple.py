d7f7961915ea93310a07a8d18b2e5c3d0034ef56
lemwaizz <mwaidennis26@gmail.com>
2023-11-08 15:19:24 -0800

    first

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     for n in matrix:
         n * n
-    result = list(map(square_matrix_simple, matrix))
+    result = map(square_matrix_simple, matrix)
     print(result)
