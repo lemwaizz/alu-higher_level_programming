add8fde7a0d0928bdbe43ca6cdbfddbc12b393b8
lemwaizz <mwaidennis26@gmail.com>
2023-11-22 12:19:33 -0800

    size

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Shebang for python 3
+"""
+
+
+class Square:
+    '''creates an empty square'''
+    def __init__(self,size):
+        """instatiate size"""
+        self.size=size
