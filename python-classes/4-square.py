1aabc835b0923b17a258ee677688d70af06178e7
lemwaizz <mwaidennis26@gmail.com>
2023-11-22 12:46:44 -0800

    @property @size.setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Shebang for python 3
+"""
+
+
+class Square:
+    '''creates an empty square'''
+    def __init__(self, size=0):
+        """instatiate size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """returns size attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """assign size to size att"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """calculates area"""
+        square_area = self.__size ** 2
+        return square_area
