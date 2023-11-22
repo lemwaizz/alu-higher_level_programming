b95eaeb9457de2ae41d8b3f9c69e9f674d33181f
lemwaizz <mwaidennis26@gmail.com>
2023-11-22 12:34:14 -0800

    area

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Shebang for python 3
+"""
+
+
+class Square:
+    '''creates an empty square'''
+    def __init__(self, size=0):
+        """instatiate size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """calculates area"""
+        square_area = self.__size ** 2
