cd55715a175842018ba64db21f1b0f3fdbb66a4f
lemwaizz <mwaidennis26@gmail.com>
2023-11-23 19:28:49 -0800

    nu

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function for pascal's triangle
+"""
+
+
+def pascal_triangle(n):
+    """function itself"""
+    n = 5
+    list = []
+    for i in range(n+1):
+        for j in range(0, n-i):
+            C = 1
+            for j in range(1, i+1):
+                C = C * (i - j) // j
+        list.append(C)
+        return list
