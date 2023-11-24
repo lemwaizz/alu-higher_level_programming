dae0ea9d14c7f24083f91ec7d4a3bf61dacae139
lemwaizz <mwaidennis26@gmail.com>
2023-11-23 17:18:19 -0800

    hey

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def is_same_class(obj, a_class):
+    """checks if obj is an instance of class
+    """
+    if obj isinstance(obj, a_class):
+        return True
+    else:
+        return False
