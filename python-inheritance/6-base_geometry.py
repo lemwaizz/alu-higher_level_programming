242d573c9113e8f746bdd97fb07d5ac212dc5b7b
lemwaizz <mwaidennis26@gmail.com>
2023-11-23 17:42:06 -0800

    exception as

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''An empty class'''
+
+
+class BaseGeometry:
+    '''Base class'''
+
+    def area(self):
+        """raising  exception
+        """
+        raise Exception("area() is not implemented")
