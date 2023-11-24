5f11d0b686d6434e5d46d3cfb94de84a628d33d0
lemwaizz <mwaidennis26@gmail.com>
2023-11-23 17:33:41 -0800

    fix

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Create function that checks if
+   object is instance of class or a class inherited from
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """function to check for the relationship
+    """
+    return isinstance(obj, a_class)
