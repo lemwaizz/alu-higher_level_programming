35d2cdf72c1511a136fab5d435abd06ed63c3552
lemwaizz <mwaidennis26@gmail.com>
2023-11-24 06:52:18 -0800

    huh

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -12,7 +12,7 @@ def pascal_triangle(n):
         for i in range(n - 1):
             temp = [0] + res + [0]
             row = []
-            for j in range(len(res[-1]) + 1):
+            for j in range(len(res) + 1):
                 row.append(temp[j] + temp[j + 1])
             res.append(row)
     return res[n - 1]
