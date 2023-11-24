fc0d858c5e67b68cd153acdb36616488de4a544d
lemwaizz <mwaidennis26@gmail.com>
2023-11-24 06:53:47 -0800

    huh

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
-"""function for pascal's triangle
-"""
+'''Pascal’s triangle of n:'''
 
 
 def pascal_triangle(n):
-    """function itself"""
-    res = []
-    if n < 0:
-        return res
-    else:
-        for i in range(n - 1):
-            temp = [0] + res + [0]
-            row = []
-            for j in range(len(res) + 1):
-                row.append(temp[j] + temp[j + 1])
-            res.append(row)
-    return res[n - 1]
+    '''returns the pascals triangle '''
+    outer_list = []
+
+    for i in range(n):
+        if i == 0:
+            outer_list.append([1])
+        elif i == 1:
+            outer_list.append([1, 1])
+        else:
+            last_list = outer_list[-1]
+            count = 0
+            new_list = [1]
+            while count < len(last_list):
+                try:
+                    new_list.append(last_list[count] + last_list[count + 1])
+                    count += 1
+                except IndexError:
+                    break
+            new_list.append(1)
+            outer_list.append(new_list)
+    return outer_list
