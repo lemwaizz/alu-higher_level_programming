2eb223aef581d8cde48338381d1e41950bb9dfc7
lemwaizz <mwaidennis26@gmail.com>
2023-11-23 18:24:30 -0800

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function to read utf8 file and print in stdout
+"""
+def read_file(filename=""):
+    """read file function
+    """
+    with open(filename, encoding = "utf-8") as f:
+        read_file = f.read()
+        print(read_file, end="")
