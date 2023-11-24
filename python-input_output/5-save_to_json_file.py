2f2318a777fe8991786fb7703d7e758e530a645b
lemwaizz <mwaidennis26@gmail.com>
2023-11-23 18:42:39 -0800

    dumps to save

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' returns object (Python data structure) represented by a JSON string:'''
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    ''' returns saves to json file'''
+    with open(filename, "w+") as f:
+        return json.dump(my_obj)
