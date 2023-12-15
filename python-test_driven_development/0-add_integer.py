322826d4cada93e08a757060e05e3fb704af9299
lemwaizz <mwaidennis26@gmail.com>
2023-12-15 13:36:37 -0800

    final fixes

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,12 +6,13 @@ def add_integer(a, b=98):
     """function for adding integers
        Args:
         a = must be an integer or a float
-        b = must be an integer or a float as well. If not provided takes 98 as a default value.
+        b = must be an integer or a float as well. If not provided takes
+        98 as a default value.
         if a is not integer raise a typeerror
     """
     if type(a) != int:
         raise TypeError("a must be an integer")
-    if type(a) != int:
+    if type(b) != int:
         raise TypeError("b must be an integer")
     if type(a) == float or type(b) == float:
         int(a)
