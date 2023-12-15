52f2a47b8be5c4b0fc92df084002eea74e181f1f
lemwaizz <mwaidennis26@gmail.com>
2023-12-15 15:17:16 -0800

    added if statement

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -7,13 +7,13 @@ def matrix_divided(matrix, div):
     for i in matrix:
         if type(i) != list:
             raise TypeError("matrix must be a matrix (list of lists)"
-                             "of integers/floats")
+                            " of integers/floats")
         if len(i) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
         for item in i:
             if not isinstance(item, (int, float)):
                 raise TypeError("matrix must be a matrix (list of lists)"
-                                 "of integers/floats")
+                                " of integers/floats")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
