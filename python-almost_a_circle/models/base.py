3a77eafd2d871ed171546116b741b40bfcb099b5
lemwaizz <mwaidennis26@gmail.com>
2024-02-27 08:43:15 -0800

    check 2

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,7 +8,7 @@ class Base:
 
     def __init__(self, id=None):
         """This is a class constructor"""
-        if id not isinstance(id, None):
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
