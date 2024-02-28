eec6ba9e281aaacc622027d7bb36755f1bb9a59c
lemwaizz <mwaidennis26@gmail.com>
2024-02-28 05:43:32 -0800

    minor fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,7 +79,7 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("y must be >= 0.")
+            raise ValueError("y must be >= 0")
         self.__y = value
 
     def update(self, *args, **kwargs):
