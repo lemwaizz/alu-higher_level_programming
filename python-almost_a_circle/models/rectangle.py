84a8e444b5d8c60375261f18fc9ba750f8effdb0
lemwaizz <mwaidennis26@gmail.com>
2024-02-28 06:27:16 -0800

    line

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -13,10 +13,10 @@ class Rectangle(Base):
         """This is a class constructor"""
 
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
