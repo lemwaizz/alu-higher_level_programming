be14f388e161653533072351b647fd67bc1f84bd
lemwaizz <mwaidennis26@gmail.com>
2024-02-28 02:33:36 -0800

    revisit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,124 +1,19 @@
 #!/usr/bin/python3
-""" This module contains the class base"""
+"""
+This is a rectangle that inherits from base
+"""
 
-from models.base import Base
+from models.base import base 
 
 
 class Rectangle(Base):
-    """ describes a rectangle
-        parameters:
-        width (int): describes the width of the rectangle
-        height (int): describes the height of the rectangle
-        x (int): coordinate
-        y (int): coordinate"""
+    """This is the rectangle class with various private attributes"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ class constructor """
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        """This is a class constructor"""
 
-    @property
-    def width(self):
-        """ width getter"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ width setter"""
-        if not isinstance(value, int):
-            raise TypeError('width must be an integer')
-        if value <= 0:
-            raise ValueError('width must be > 0')
-        self.__width = value
-
-    @property
-    def height(self):
-        """ height getter"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ height setter """
-        if not isinstance(value, int):
-            raise TypeError('height must be an integer')
-        if value <= 0:
-            raise ValueError('height must be > 0')
-        self.__height = value
-
-    @property
-    def x(self):
-        """ x getter """
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """ x setter """
-        if not isinstance(value, int):
-            raise TypeError('x must be an integer')
-        if value < 0:
-            raise ValueError('x must be >= 0')
-        self.__x = value
-
-    @property
-    def y(self):
-        """ y getter """
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """ y setter """
-        if not isinstance(value, int):
-            raise TypeError('y must be an integer')
-        if value < 0:
-            raise ValueError('y must be >= 0')
-        self.__y = value
-
-    def area(self):
-        """ calculates the area of the rectangle"""
-        return self.width * self.height
-
-    def display(self):
-        """ displays the rectangle using #"""
-        for _ in range(self.y):
-            print()
-        for _ in range(self.height):
-            print(" " * self.x + "#" * self.width)
-
-    def __str__(self):
-        """ overrides the __str__ method """
-        return f"[Rectangle] ({self.id})"\
-               f" {self.x}/{self.y} - {self.width}/{self.height}"
-
-    def update(self, *args):
-        """ update arguments """
-        if args != 0:
-            try:
-                self.id = args[0]
-                self.width = args[1]
-                self.height = args[2]
-                self.x = args[3]
-                self.y = args[4]
-
-            except IndexError:
-                pass
-        elif len(kwargs) != 0:
-            self.id = kwargs["id"] if "id" in kwargs else self.id
-            self.width = kwargs["width"] if "width" in kwargs \
-                else self.width
-            self.height = kwargs["height"] if "height" in kwargs \
-                else self.height
-            self.x = kwargs["x"] if "x" in kwargs else self.x
-            self.y = kwargs["y"] if "y" in kwargs else self.y
-
-    def to_dictionary(self):
-        """dictionary representation"""
-        return {
-            'id': self.id,
-            'width': self.width,
-            'height': self.height,
-            'x': self.x,
-            'y': self.y
-        }
+        super.__init__(self, id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
